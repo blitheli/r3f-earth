004c380ebb2cb7596c731bc177f3db6fc8172b5c
yunfei li <47636962@qq.com>
2026-03-23 14:14:17 +0800

    修复GlobeTest.tsx

--- src/pages/GlobeTest.tsx
+++ src/pages/GlobeTest.tsx
@@ -7,17 +7,16 @@ import { CesiumGlobe } from "../components/CesiumGlobe";
 /*
 
   测试东京是可以的
+  若为Cesium
 */
 
 // Aerometrex San Francisco High Resolution 3D Model with Street Level Enhanced 3D (Non-Commercial Trial)
-const assetId = 1415196; //2275207
-//const assetId = 2275207;
+//const assetId = 1415196; //2275207
+const assetId = 2275207;    // Cesium全球, fps不高，也不流畅
 const apiToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4M2ZhYzM4My1lN2NhLTRjNTktODY1OC1jZDdmOTU3Y2ZjMGEiLCJpZCI6MTMwNTAsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjI0NzA5NzB9.rRTs6chsWJdo9KNYe5VjJj2fUzMHeniIJvFQOd0aLJU";
 
 export default function GlobeTest() {
-  //  加载glb模型,并使用Drei的html标注进行兴趣点标注
-  //-----------------------------------------------------------------
   return (
     <WebGPUCanvas
       frameloop="demand"
@@ -28,11 +27,10 @@ export default function GlobeTest() {
       }}
       camera={{
         fov: 60,
-        // position: [-2e7, 0, 0],
-        position: [300, 300, 300],
-        // up: [0, 0, 1],
-        near: 1,
-        far: 1e5,
+        position: [-2e7, 0, 0],  // 距离地心 2000 万米
+        //up: [0, 0, 1],            // Z 轴向上
+        near: 1e3,                // 1 公里 - 防止太近时出现精度问题
+        far: 1e9,                 // 10 亿米 - 足够看到整个地球
       }}
     >
       <CesiumGlobe
@@ -40,8 +38,12 @@ export default function GlobeTest() {
         assetId={assetId as number}
       />
 
-      {/* Controls */}
-      <EnvironmentControls enableDamping={true} maxDistance={5000} />
+      {/* Controls - 允许更近的距离 */}
+      <EnvironmentControls 
+        enableDamping={true} 
+        minDistance={1e3}      // 最近 1 公里
+        maxDistance={1e8}      // 最远 1 亿米
+      />
 
       {/* other r3f staging */}
       <Environment
